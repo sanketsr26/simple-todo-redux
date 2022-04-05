319cf65df40fc56b47f6e4424305046c561e2c4a
Rapkal, Sanket <sanketrapkal47@gmail.com>
2022-04-05 14:54:01 +0530

    setting up store, adding reducer for adding todo item

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -5,7 +5,7 @@ const TodoItem = ({ id, title, completed }) => {
     <li className={`list-group-item ${completed && "list-group-item-success"}`}>
       <div className="d-flex justify-content-between">
         <span className="d-flex align-items-center">
-          <input type="checkbox" className="mr-3" checked={completed}></input>
+          <input onChange={() => console.log("clicked")} type="checkbox" className="mr-3" checked={completed}></input>
           {title}
         </span>
         <button className="btn btn-danger">Delete</button>
